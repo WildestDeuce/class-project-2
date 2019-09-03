41b5f6af99874599d72345ec67f51f6f28bc6024
Ashlie Johnson <ashlie86.2003@yahoo.com>
2019-09-03 19:13:01 -0400

    added extra text field for password

--- models/schema.sql
+++ models/schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE channel (
     messages varchar(255),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT,
+    handle varchar(50) NOT NULL,
+    password longtext,
+    email varchar(50) Not Null,
+    PRIMARY KEY(id)
+);
