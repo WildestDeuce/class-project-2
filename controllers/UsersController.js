ce7546b55acab49551faed6a2248da8d69946366
Brady Douthit <brady.douthit99@gmail.com>
2019-09-07 08:41:47 -0400

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,18 +1,18 @@
-var express = require("express");
-var router = express.Router();
+// var express = require("express");
+// var router = express.Router();
 
-var db = require("../models");
+// var db = require("../models");
 
-router.get("/", function(req, res) {
-    res.redirect("/");
-});
+// router.get("/", function(req, res) {
+//     res.redirect("/");
+// });
 
-router.post("/users/create", function (req, res) {
-    db.user.create({
-        email: req.params.email, handle: req.params.handle, password: req.params
-    })
-    .then(function(dbUser) {
-        console.log(dbUser);
-        res.redirect("/")
-    })
-})
+// router.post("/users/create", function (req, res) {
+//     db.user.create({
+//         email: req.params.email, handle: req.params.handle, password: req.params
+//     })
+//     .then(function(dbUser) {
+//         console.log(dbUser);
+//         res.redirect("/")
+//     })
+// })
